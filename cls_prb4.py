1164127b3cb0169bdaa8168ce5b07affd2b916d8
sanmati <sanmatimorbad@gmail.com>
2019-01-13 16:41:38 +0530

    Program to print a statement in reverse order using class

---
+++ cls_prb4.py
@@ -0,0 +1,12 @@
+
+class reversestr:
+	def stringrev():
+		str1=input("Enter a statement")
+		lst1=str1.split()
+		for i in range(1,len(lst1)+1):
+			lst2.append(lst1[len(lst1)-i])
+		str2=" ".join(lst2)
+		print(str2)
+lst1=[]
+lst2=[]
+reversestr.stringrev()
