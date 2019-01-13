593e1767571dd8833ea9094361c57cee3442fedc
sanmati <sanmatimorbad@gmail.com>
2019-01-13 15:19:25 +0530

    Program to print numbers less than 5 in list using class

--- cls_prb2.py
+++ cls_prb2.py
@@ -1,10 +1,13 @@
+class numb:
+	def printnum(n):
+		n=int(input("enter number of elements in list"))
+		for i in range(n):
+			lst1.extend(input("Enter input numbers"))
+		for num in lst1:
+			if int(num)<5:
+				lst2.append(num)
+		print(lst2)
+
 lst1=[]
 lst2=[]
-n=int(input("enter number of elements in list"))
-for i in range(n):
-	lst1.extend(input("Enter input numbers"))
-for num in lst1:
-	if int(num)<5:
-		lst2.append(num)
-
-print(lst2)
+numb.printnum(3)
