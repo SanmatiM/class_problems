bc5fd6082a6ffe7e2e020bd3d0b69d55589cfa34
sanmati <sanmatimorbad@gmail.com>
2019-01-13 16:19:59 +0530

    Program to create dictionary of name and usn using class

--- cls_prb6.py
+++ cls_prb6.py
@@ -1,10 +1,14 @@
+class Student:
+	def details():
+		for n in range(3):
+			n=input("Enter name")
+			name.append(n)
+			u=input("enter usn")
+			usn.append(u)
+		d=dict(zip(usn,name))
+		print(d)
+
 d=dict()
 name=[]
 usn=[]
-for n in range(3):
-	n=input("Enter name")
-	name.append(n)
-	u=input("enter usn")
-	usn.append(u)
-d=dict(zip(usn,name))
-print(d)
+Student.details()
