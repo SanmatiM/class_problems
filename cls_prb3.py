06d371b38227c9041d1eed328f084aef7ce58cca
sanmati <sanmatimorbad@gmail.com>
2019-01-13 15:22:09 +0530

    Program to print numbers which are not repeated in a list using class

--- cls_prb3.py
+++ cls_prb3.py
@@ -1,9 +1,12 @@
+class element:
+	def elenotrep(n):
+		n=int(input("enter number of elements in list"))
+		for i in range(n):
+			lst1.extend(input("Enter input numbers"))
+		for num in lst1:
+			if num not in lst2:
+				lst2.append(num)
+		print(lst2)
 lst1=[]
 lst2=[]
-n=int(input("enter number of elements in list"))
-for i in range(n):
-	lst1.extend(input("Enter input numbers"))
-for num in lst1:
-	if num not in lst2:
-		lst2.append(num)
-print(lst2)
+element.elenotrep(3)
