2b0eb8fdba955203ba0965e8aea8a386af74e404
sanmati <sanmatimorbad@gmail.com>
2019-01-13 15:18:14 +0530

    Program to print age after 100 years using class

--- cls_prb1.py
+++ cls_prb1.py
@@ -1,3 +1,6 @@
+class Name:
+	def printn(name,age):
+		print("Hey %s! You’ll turn 100 years old in %d"%(name,2019-age+100))
 name=input("Enter full name")
 age=int(input("Enter age"))
-print("Hey %s! You’ll turn 100 years old in %d"%(name,2019-age+100))
+Name.printn(name,age)
